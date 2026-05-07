3fa09485b9760b5a723d20c370d10de580dc25e9
Aayush Chowdhury <Aayush.Chowdhury@Cognizant.com>
2026-05-07 08:43:07 +0530

    DBA Data SQL File

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users (Name, Email, Password, PaymentDetails, Role)
+VALUES('DB_ADMIN', 'dba@ecommerce.com', '8f51ca69872d9c8f52856464e87635816b9b3e6c4b08112802ac3a09e692903f', null, 'Admin');
+
+SELECT * FROM Users;
